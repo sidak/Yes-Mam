45b1c8ddec6baf36de01e3e82568c37c6fe52a83
sidak <spssachdeva21695@gmail.com>
2014-07-02 18:36:32 +0530

    add splashscreen activity and .gitignore

---
+++ YesMam!!/src/com/sidak/yesmam/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.sidak.yesmam;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashScreenActivity extends Activity {
+	
+	private static int SPLASH_TIME_OUT = 1500;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// call main activity
+				Intent intent= new Intent(SplashScreenActivity.this, MainActivity.class);
+				// if u directly use 'this' for context, then this refers to the
+				// runnable and not the splashscreen activity
+				startActivity(intent);
+				finish();
+				
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
