ed722531ad6df8ade28c151a8b26ead5b5244f7f
sidak <spssachdeva21695@gmail.com>
2014-07-25 02:04:10 +0530

    add timepickerfragment and refactor the layout elements

---
+++ YesMam!!/src/com/sidak/yesmam/TimePickerFragment.java
@@ -0,0 +1,39 @@
+package com.sidak.yesmam;
+
+import java.util.Calendar;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+@SuppressLint("ValidFragment")
+public class TimePickerFragment extends DialogFragment implements
+		TimePickerDialog.OnTimeSetListener {
+	private TextView textView;
+	public TimePickerFragment(TextView tv){
+		textView=tv;
+	}
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current time as the default values for the picker
+		final Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+
+		// Create a new instance of TimePickerDialog and return it
+		return new TimePickerDialog(getActivity(), this, hour, minute,
+				DateFormat.is24HourFormat(getActivity()));
+	}
+
+	@Override
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+		// TODO Auto-generated method stub
+		textView.setText(String.valueOf(hourOfDay)+":"+String.valueOf(minute));
+
+	}
+}
