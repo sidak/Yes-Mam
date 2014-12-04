773e4740e2b0ecfa755052544bf05e3f9a06b052
sidak <spssachdeva21695@gmail.com>
2014-12-05 02:18:19 +0530

    use shared preferences for selecting the initial activity to open

--- YesMam!!/src/com/sidak/yesmam/SplashScreenActivity.java
+++ YesMam!!/src/com/sidak/yesmam/SplashScreenActivity.java
@@ -2,30 +2,48 @@ package com.sidak.yesmam;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
 
 public class SplashScreenActivity extends Activity {
-	
+
 	private static int SPLASH_TIME_OUT = 1500;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		boolean previouslyStarted = prefs.getBoolean(
+				getString(R.string.prefPreviouslyStarted), false);
+		if (!previouslyStarted) {
+			SharedPreferences.Editor edit = prefs.edit();
+			edit.putBoolean(getString(R.string.prefPreviouslyStarted),
+					Boolean.TRUE);
+			edit.commit();
+			runDelayedNextActivity(SemesterActivity.class);
+		} else {
+			runDelayedNextActivity(MainActivity.class);
+
+		}
+	}
+
+	private void runDelayedNextActivity(final Class c) {
 		new Handler().postDelayed(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// call main activity
-				Intent intent= new Intent(SplashScreenActivity.this, SemesterActivity.class);
+				Intent intent = new Intent(SplashScreenActivity.this, c);
 				// if u directly use 'this' for context, then this refers to the
 				// runnable and not the splashscreen activity
 				startActivity(intent);
 				finish();
-				
+
 			}
 		}, SPLASH_TIME_OUT);
 	}
