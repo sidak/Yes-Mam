373e781909a091248c08e489604c87b2d99ea383
sidak <spssachdeva21695@gmail.com>
2014-12-17 03:49:49 +0530

    create datasource and sqliteOpenHelper classes for working days

---
+++ YesMam!!/src/com/sidak/yesmam/db/WDBOpenHelper.java
@@ -0,0 +1,69 @@
+package com.sidak.yesmam.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class WDBOpenHelper extends SQLiteOpenHelper {
+
+	public static final String TAG = "YES MAM  W DB";
+	private static int NUM_COURSES=0;
+	private static final String DATABASE_NAME = "wdays.db";
+	private static final int DATABASE_VERSION = 1;
+
+	public static void setNumCourses(int n) {
+		NUM_COURSES = n;
+	}
+
+	public static final String TABLE_WDAYS = "workingDays";
+	public static String[] COURSES;
+	public static String[] ATTENDANCE;
+	public static final String WDAY_DATE = "date";
+	public static final String WDAY_DAY = "day";
+	public static final String WDAY_ID = "wdayId";
+
+	private static String TABLE_CREATE_WDAYS = "CREATE TABLE " + TABLE_WDAYS
+			+ " (" + WDAY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ WDAY_DATE + " TEXT, " + WDAY_DAY + " TEXT, ";
+
+	public WDBOpenHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		Log.i(TAG, "dbopen helper constr");
+
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// db.execSQL(TABLE_CO);
+		Log.i(TAG, "ewcvkewnfcknk");
+		Log.i(TAG, TABLE_CREATE_WDAYS);
+		COURSES= new String[NUM_COURSES];
+		ATTENDANCE=new String[NUM_COURSES];
+		for (int i = 0; i < NUM_COURSES; i++) {
+			COURSES[i] = "course" + i;
+		}
+		for (int i = 0; i < NUM_COURSES; i++) {
+			ATTENDANCE[i] = "attendance" + i;
+		}
+		for (int i = 0; i < NUM_COURSES; i++) {
+			TABLE_CREATE_WDAYS += (COURSES[i] + " TEXT, ");
+		}
+		for (int i = 0; i < NUM_COURSES-1; i++) {
+			TABLE_CREATE_WDAYS += (ATTENDANCE[i] + " INTEGER, ");
+		}
+		TABLE_CREATE_WDAYS += (ATTENDANCE[NUM_COURSES-1] + " INTEGER ");
+		TABLE_CREATE_WDAYS += ")";
+		db.execSQL(TABLE_CREATE_WDAYS);
+
+		Log.i(TAG, "Table has been created");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_WDAYS);
+		onCreate(db);
+	}
+
+}
---
+++ YesMam!!/src/com/sidak/yesmam/db/WorkingDaysDataSource.java
@@ -0,0 +1,104 @@
+package com.sidak.yesmam.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.sidak.yesmam.model.Course;
+import com.sidak.yesmam.model.WorkingDay;
+
+public class WorkingDaysDataSource {
+	public static int NUM_COURSES;
+	public static final String TAG = WorkingDaysDataSource.class.getSimpleName();
+
+	SQLiteOpenHelper dbhelper;
+	SQLiteDatabase database;
+	private static  String[] allColumns; 
+	public WorkingDaysDataSource(Context context, int n) {
+		NUM_COURSES=n;
+		WDBOpenHelper.setNumCourses(n);
+		dbhelper = new WDBOpenHelper(context);
+		
+		allColumns= new String[3+2*NUM_COURSES];
+		allColumns[0]=WDBOpenHelper.WDAY_ID;
+		allColumns[1]=WDBOpenHelper.WDAY_DATE;
+		allColumns[2]=WDBOpenHelper.WDAY_DAY;
+		Log.v(TAG, "constructor course data src");
+	}
+
+	public void open() {
+		Log.i(TAG, "Course Database opened");
+		database = dbhelper.getWritableDatabase();
+			
+		for(int i=0; i<NUM_COURSES;i++){
+			allColumns[3+i]=WDBOpenHelper.COURSES[i];
+		}
+		for(int i=0; i<NUM_COURSES;i++){
+			allColumns[3+NUM_COURSES+i]=WDBOpenHelper.ATTENDANCE[i];
+		}
+	}
+
+	public void close() {
+		Log.i(TAG, "Database closed");
+		dbhelper.close();
+	}
+
+	public void create(WorkingDay w) {
+		// TODO Auto-generated method stub
+		ContentValues values = new ContentValues();
+		values.put(WDBOpenHelper.WDAY_DATE, w.getDateString());
+		values.put(WDBOpenHelper.WDAY_DAY, w.getDayString());
+		for(int i=0; i<NUM_COURSES;  i++){
+			values.put(WDBOpenHelper.COURSES[i],w.courses.get(i).getCourseCode());
+		}
+		for(int i=0; i<NUM_COURSES;  i++){
+			values.put(WDBOpenHelper.ATTENDANCE[i],w.attendance.get(i));
+		}
+		long insertid = database.insert(WDBOpenHelper.TABLE_WDAYS, null,
+				values);
+		Log.i(TAG, "in create in datasrc " + insertid);
+	}
+
+	public List<WorkingDay> findAll() {
+
+		Cursor cursor = database.query(WDBOpenHelper.TABLE_WDAYS, allColumns,
+				null, null, null, null, null);
+
+		Log.i(TAG, "Returned " + cursor.getCount() + " rows");
+		List<WorkingDay> w = cursorToList(cursor);
+		return w;
+	}
+
+	private List<WorkingDay> cursorToList(Cursor cursor) {
+		List<WorkingDay> wdays = new ArrayList<WorkingDay>();
+		if (cursor.getCount() > 0) {
+			while (cursor.moveToNext()) {
+				WorkingDay wday = new WorkingDay();
+				wday.setDateString(cursor.getString(cursor
+						.getColumnIndex(WDBOpenHelper.WDAY_DATE)));
+				wday.setDayString(cursor.getString(cursor
+						.getColumnIndex(WDBOpenHelper.WDAY_DAY)));
+				wday.courses = new ArrayList<Course>(NUM_COURSES);
+				for(int i=0; i<NUM_COURSES;  i++){
+					wday.courses.add(new Course(cursor.getString(cursor.getColumnIndex(WDBOpenHelper.COURSES[i]))));
+				}
+				wday.attendance=new ArrayList<Integer>(NUM_COURSES);
+				for(int i=0; i<NUM_COURSES;  i++){
+					wday.attendance.add(cursor.getInt(cursor.getColumnIndex(WDBOpenHelper.ATTENDANCE[i])));
+				}
+				
+				wdays.add(wday);
+				Log.v(TAG, wday.toString());
+
+			}
+		}
+		return wdays;
+	}
+
+}
